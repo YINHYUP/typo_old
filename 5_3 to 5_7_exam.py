719c73f99dc560c5592cff4e7c34528525cbb686
YINHYUP <149332863+YINHYUP@users.noreply.github.com>
2024-01-02 21:45:33 +0000

    test

---
+++ 5_3 to 5_7_exam.py
@@ -0,0 +1,24 @@
+# 5_3 외계인 색깔#1
+
+enemy=["green","yellow","red"]
+
+killed=input("enter enemy color you burst!!")
+
+point=0
+sumpoint=0
+  
+cycle=len(enemy)
+
+for cycle > 1
+    if killed in enemy:
+
+        if killed == "green":
+            point = 5
+            sumpoint=sumpoint+point
+            print(f"You Kill {killed} enemy. You got {point} point!! Your total Point is {sumpoint}")
+            enemy.remove(killed)
+        else:
+            print(f"You Kill {killed} enemy. You connot get point. Your total Point is {sumpoint}")
+    else :
+        print("You are die.")
+    
---
+++ test.py
@@ -0,0 +1,16 @@
+num=int(input("주민등록번호를 입력해주세요."))
+y=num //10000
+
+if y>24:
+    y=2024-(y+1900)
+else :
+    y=2024-(y+2000)
+
+if y<12 :
+    cost = ("free")
+elif y<18:
+    cost = ("10 USD")
+else:
+    cost = ("20 USD")
+    
+print(f"your age is {y}. your Entrance Fee is {cost}.")
