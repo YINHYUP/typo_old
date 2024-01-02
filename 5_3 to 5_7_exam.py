a2c36b499e2ac28eec54330248986cb929af1ede
YINHYUP <149332863+YINHYUP@users.noreply.github.com>
2024-01-03 06:56:49 +0900

    test

--- 5_3 to 5_7_exam.py
+++ 5_3 to 5_7_exam.py
@@ -1,24 +1,20 @@
-# 5_3 외계인 색깔#1
+# 5_3 외계인 색깔
 
 enemy=["green","yellow","red"]
-
-killed=input("enter enemy color you burst!!")
-
 point=0
 sumpoint=0
-  
-cycle=len(enemy)
+cycle=len(enemy)  
 
-for cycle > 1
+if cycle > 1:
+    killed=input("enter enemy color you bursted!!")
     if killed in enemy:
-
         if killed == "green":
             point = 5
             sumpoint=sumpoint+point
             print(f"You Kill {killed} enemy. You got {point} point!! Your total Point is {sumpoint}")
             enemy.remove(killed)
         else:
-            print(f"You Kill {killed} enemy. You connot get point. Your total Point is {sumpoint}")
-    else :
-        print("You are die.")
+            print(f"You Kill {killed} enemy. You connot get point. Your total Point is {sumpoint}")    
+else :
+    print("You are die.")
     
