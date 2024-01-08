8232652ad97d411c0c5ebecc4f7b92f0b164b6f1
YINHYUP <149332863+YINHYUP@users.noreply.github.com>
2024-01-08 22:12:53 +0000

    파이썬 공부중_240109

--- python_stydy/6_dictionary.py
+++ python_stydy/6_dictionary.py
@@ -1,4 +1,51 @@
+"""
+
 alien_0 = {'color' : 'green', 'point' : 5}
 
 print(alien_0['color'])
 print(alien_0['point'])
+
+"""
+
+"""
+
+alien_0 = {'color' : 'green', 'point' : 5}
+
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+
+"""
+
+"""
+
+alien_0 = {'color' : 'green'}
+print(f"Alien color is {alien_0['color']}.")
+alien_0['color'] = 'red'
+print(f"Now alien color is {alien_0['color']}.")
+
+"""
+
+alien_0 = {'x_position' : 0, 'y_position' : 25, 'speed' : 'fast'}
+
+print(f"Original alien position : {alien_0['x_position']}, {alien_0['y_position']}")
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+    y_increment = 2
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+    y_increment = 4
+elif alien_0['speed'] == 'fast':
+    x_increment = 3
+    y_increment = 6
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+alien_0['y_position'] = alien_0['y_position'] + y_increment
+
+print(f"New alien position : {alien_0['x_position']}, {alien_0['y_position']}")
+
+
