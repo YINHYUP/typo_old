da7b7ef11e92a51cfd01a9c08a869d07232cf343
YINHYUP <149332863+YINHYUP@users.noreply.github.com>
2024-01-08 21:48:03 +0000

    파이썬 공부중

--- python_stydy/5_4_2.py
+++ python_stydy/5_4_2.py
@@ -1,16 +1,47 @@
-c_users=["a","b","c","d","e","admin"]
-n_users=["f","g","a","c","z"]
+"""
+c_users=["A","B","C","D","E","ADMIN"]
+n_users=["f","g","A","c","Z"]
+c_users_lower=[name.lower() for name in c_users]
+n_users_lower=[name.lower() for name in n_users]
 
 
-if c_users:
-    for user in n_users:
-        if user in c_users:
+
+if c_users_lower:
+    for user in n_users_lower:
+        if user in c_users_lower:
             print(f"{user} is already used name. Please use other name.")
         else:
             print(f"user name {user} is possible to use.")
 else:
     print("We need user.")
 
+"""
 
+numbers=list(range(1,10))
+numbers_2=[]
+print(numbers)
+
+"""
+for number in numbers:
+    if number == 1:
+        print(f"{number}st")
+    elif number == 2:
+        print(f"{number}nd")
+    else:
+        print(f"{number}th")
+"""
+for number in numbers:
+    if number == 1:
+        numbers_2.append ( f"{number}st" )
+        print(numbers)
+        print(numbers_2)
+    elif number == 2:
+        numbers_2.append ( f"{number}nd" )
+        print(numbers)
+        print(numbers_2)
+    else:
+        numbers_2.append ( f"{number}st" )
+        print(numbers)
+        print(numbers_2)
 
 
